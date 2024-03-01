42521ef2f546bf2e671258d2e228ffa8581a2541
garbagetrees <f.skatt.fsc@gmail.com>
2024-03-01 14:44:27 +0100

    added Hello World

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello World");
     }
 
     // Update is called once per frame
