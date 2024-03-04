23247228cfeb2ff6e6bf3b11b5ff135c0e2146e6
garbagetrees <f.skatt.fsc@gmail.com>
2024-03-04 12:22:54 +0100

    Added Mainmenu and player movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-
+    Rigidbody2D rb;
     GameObject Enemy;
-
+    public float movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
         Enemy = GameObject.Find("Enemy");
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float output = Mathf.Sqrt(Mathf.Pow(Enemy.transform.position.x - this.gameObject.transform.position.x, 2.0f) + Mathf.Pow(Enemy.transform.position.y - this.gameObject.transform.position.y, 2.0f));
         print(output);
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.up*movementSpeed,ForceMode2D.Force);
+            
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            rb.AddForce(-transform.up * movementSpeed, ForceMode2D.Force);
+            
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * movementSpeed, ForceMode2D.Force);
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * movementSpeed, ForceMode2D.Force);
+        
+        }
     }
 }
