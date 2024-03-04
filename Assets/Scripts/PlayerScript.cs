75a534a409752774405bb1617884f73a0e28c401
garbagetrees <f.skatt.fsc@gmail.com>
2024-03-04 09:00:53 +0100

    Hello World

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    float movementSpeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        this.gameObject.transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
     }
 }
