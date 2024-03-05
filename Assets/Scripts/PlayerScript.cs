bbdc51d4c3b22a93bee7d9980137d71a85d736ec
garbagetrees <f.skatt.fsc@gmail.com>
2024-03-05 20:41:17 +0100

    removed redundancy

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -38,10 +38,6 @@ public class PlayerScript : MonoBehaviour
             rb.AddForce(transform.right * movementSpeed, ForceMode2D.Force);
         
         }
-        else if (Input.GetKey(KeyCode.Escape))
-        {
-            // do thing
-
-        }
+        
     }
 }
