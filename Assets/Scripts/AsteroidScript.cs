8c8e91ee5d5f693a7e8975c7b8a30ad60e9113d6
Zync <Lukasjuulnielsen@gmail.com>
2024-03-11 19:33:43 +0100

    AsteroidScript

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 2f * Mathf.Cos(1.5f * Time.deltaTime);
+        float y = 2f * Mathf.Sin(1.5f * Time.deltaTime);
+
+        print(x + " - " + y);
+
+        this.transform.position += new Vector3(x, y);
+    }
+}
